66bcf09b3de30dccda6e36324c18d76c5b7b860a
Klaas Schoute <hello@student-techlife.com>
2024-05-13 23:37:09 +0200

    Fix error about getConfig is undefined (#5)

--- stream_overlays/__init__.py
+++ stream_overlays/__init__.py
@@ -14,11 +14,11 @@ def initialize(rhapi):
     @bp.route('/stream/overlay/<string:name>/node/<int:node_id>')
     def render_node_overlay(name: str, node_id: int):
         """Render the node overlay."""
-        return templating.render_template(f'stream/overlays/node_{name}.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, node_id=node_id-1)
+        return templating.render_template(f'stream/overlays/node_{name}.html', serverInfo=None, getOption=rhapi.db.option, getConfig=rhapi.config.get_item, __=rhapi.__, node_id=node_id-1)
 
     @bp.route('/stream/overlay/<string:name>/topbar')
     def render_topbar_overlay(name: str):
         """Render the topbar overlay."""
-        return templating.render_template(f'stream/topbars/topbar_{name}.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__)
+        return templating.render_template(f'stream/topbars/topbar_{name}.html', serverInfo=None, getOption=rhapi.db.option, getConfig=rhapi.config.get_item, __=rhapi.__)
 
     rhapi.ui.blueprint_add(bp)
