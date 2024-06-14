915b55cb91ef648af358fb28e1e2b15e312d2ad0
Klaas Schoute <klaas_schoute@hotmail.com>
2024-06-14 21:23:25 +0000

    Run prettier code formatter

--- stream_overlays/__init__.py
+++ stream_overlays/__init__.py
@@ -1,11 +1,13 @@
 """DDS - RotorHazard Stream Overlay Plugin"""
+
 from eventmanager import Evt
 from flask import templating
 from flask.blueprints import Blueprint
 
 overlays: list = ["DDS", "LCDR"]
 
-class StreamOverlays():
+
+class StreamOverlays:
     """Stream Overlays plugin class."""
 
     def __init__(self, rhapi):
@@ -30,18 +32,25 @@ class StreamOverlays():
 
             # Register a panel for each overlay on the streams page
             panel_id = f"stream_overlays_{overlay_name.lower()}"
-            self._rhapi.ui.register_panel(panel_id, f"{overlay_name} - OBS Overlays", "streams")
+            self._rhapi.ui.register_panel(
+                panel_id, f"{overlay_name} - OBS Overlays", "streams"
+            )
 
             # Create header, link and markdown block for the topbar
             topbar_markdown: str = "## Topbar\n"
             topbar_markdown += f"- <a href='{base_path}/topbar' target='_blank'>{overlay_name} Overlay - Topbar</a>\n"
-            self._rhapi.ui.register_markdown(panel_id, f"{overlay_name}-Topbar", topbar_markdown)
+            self._rhapi.ui.register_markdown(
+                panel_id, f"{overlay_name}-Topbar", topbar_markdown
+            )
 
             # Create header, links and markdown block for each node overlay (or mock nodes)
             nodes_markdown: str = "## Nodes\n"
             for i in range(num_nodes):
                 nodes_markdown += f"- <a href='{base_path}/node/{i+1}' target='_blank'>{overlay_name} Overlay - Node {i+1}</a>\n"
-            self._rhapi.ui.register_markdown(panel_id, f"{overlay_name}-Nodes", nodes_markdown)
+            self._rhapi.ui.register_markdown(
+                panel_id, f"{overlay_name}-Nodes", nodes_markdown
+            )
+
 
 def initialize(rhapi):
     """Initialize the plugin.
@@ -56,31 +65,58 @@ def initialize(rhapi):
     rhapi.events.on(Evt.STARTUP, stream_overlays.create_panels)
 
     bp = Blueprint(
-        'stream_overlays',
+        "stream_overlays",
         __name__,
-        template_folder='pages',
-        static_folder='static',
-        static_url_path='/stream_overlays/static'
+        template_folder="pages",
+        static_folder="static",
+        static_url_path="/stream_overlays/static",
     )
 
-    @bp.route('/stream/overlay/<string:name>/node/<int:node_id>')
+    @bp.route("/stream/overlay/<string:name>/node/<int:node_id>")
     def render_node_overlay(name: str, node_id: int):
         """Render the node overlay."""
-        return templating.render_template(f'stream/overlays/node_{name}.html', serverInfo=None, getOption=rhapi.db.option, getConfig=rhapi.config.get_item, __=rhapi.__, node_id=node_id-1)
+        return templating.render_template(
+            f"stream/overlays/node_{name}.html",
+            serverInfo=None,
+            getOption=rhapi.db.option,
+            getConfig=rhapi.config.get_item,
+            __=rhapi.__,
+            node_id=node_id - 1,
+        )
 
-    @bp.route('/stream/overlay/<string:name>/topbar')
+    @bp.route("/stream/overlay/<string:name>/topbar")
     def render_topbar_overlay(name: str):
         """Render the topbar overlay."""
-        return templating.render_template(f'stream/topbars/topbar_{name}.html', serverInfo=None, getOption=rhapi.db.option, getConfig=rhapi.config.get_item, __=rhapi.__)
+        return templating.render_template(
+            f"stream/topbars/topbar_{name}.html",
+            serverInfo=None,
+            getOption=rhapi.db.option,
+            getConfig=rhapi.config.get_item,
+            __=rhapi.__,
+        )
 
-    @bp.route('/stream/overlay/<string:name>/leaderboard/<int:class_id>/overall')
+    @bp.route("/stream/overlay/<string:name>/leaderboard/<int:class_id>/overall")
     def render_overall_class_overlay(name: str, class_id: int):
         """Render the overall class leaderboard overlay."""
-        return templating.render_template(f'stream/leaderboard/{name}/overall.html', serverInfo=None, getOption=rhapi.db.option, getConfig=rhapi.config.get_item, __=rhapi.__, class_id=class_id)
+        return templating.render_template(
+            f"stream/leaderboard/{name}/overall.html",
+            serverInfo=None,
+            getOption=rhapi.db.option,
+            getConfig=rhapi.config.get_item,
+            __=rhapi.__,
+            class_id=class_id,
+        )
 
-    @bp.route('/stream/overlay/<string:name>/leaderboard/<int:class_id>/class')
+    @bp.route("/stream/overlay/<string:name>/leaderboard/<int:class_id>/class")
     def render_class_leaderboard_overlay(name: str, class_id: int):
         """Render the class leaderboard overlay."""
-        return templating.render_template(f'stream/leaderboard/{name}/class.html', serverInfo=None, getOption=rhapi.db.option, getConfig=rhapi.config.get_item, __=rhapi.__, class_id=class_id)
+        return templating.render_template(
+            f"stream/leaderboard/{name}/class.html",
+            serverInfo=None,
+            getOption=rhapi.db.option,
+            getConfig=rhapi.config.get_item,
+            __=rhapi.__,
+            class_id=class_id,
+        )
 
     rhapi.ui.blueprint_add(bp)
